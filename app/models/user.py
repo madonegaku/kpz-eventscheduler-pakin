51971f83d3ce7863fc18d9986eb0b583d825656d
Denis Pakin <madonegakuen@gmail.com>
2026-04-24 06:57:57 +0300

    docs: додати опис архітектури автентифікації

--- app/models/user.py
+++ app/models/user.py
@@ -2,5 +2,5 @@
 class User:
     def __init__(self, email, password):
         self.email = email
-        self.password = password
+        self.password = password # У проєкті тут буде хеш
         self.is_active = True
