c55ff3bf6b0faa40d8d638cf77f6db739a6ec8ec
Denis Pakin <madonegakuen@gmail.com>
2026-05-08 10:52:49 +0300

    docs: додати архітектурну схему та ADR-001

--- app/models/user.py
+++
@@ -1,6 +0,0 @@
-
-class User:
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password # У проєкті тут буде хеш
-        self.is_active = True
---
+++ src/models/event.py
@@ -0,0 +1,11 @@
+# src/models/event.py
+from datetime import datetime
+
+class Event:
+    def __init__(self, title, start_time, description=""):
+        self.title = title
+        self.start_time = start_time
+        self.description = description
+        self.created_at = datetime.now()
+    def is_upcoming(self):
+        return self.start_time > datetime.now()
